51694b2d38ea2f350fb0498cb38229d00363774a
st-prasad <trindo.me@gmail.com>
2023-05-22 23:24:36 +0530

    cleaned comments

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,3 @@
-// import Link from 'next/link';
-// import { PostMetaDataType } from '../logicComp/Interfaces';
-// import getPostMetaData from '../logicComp/getPostMetadata';
-// import PostsTable from './components/PostsTable';
-
-// export default function HomePage() {
-//   const postMetaData = getPostMetaData();
-//   const postPreviews = postMetaData.map((metaDataObj: PostMetaDataType) => { //mapping the slugs to the posts
-//     return (
-//       <PostsTable key={metaDataObj.slug} {...metaDataObj} />
-//       // <PostsTable key="metaDataObj.slug" date='metaDataObj.date' title='metaDataObj.title' subtitle='metaDataObj.subtitle' slug='metaDataObj.slug' />
-//     )
-//   });
-
-
-//   return (
-//     <div className='p-4'>{postPreviews}</div>
-//   )
-// }
-
 import React from 'react'
 import PostsTable from './components/PostsTable'
 
