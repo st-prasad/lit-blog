41f8cb5afea507ad491a0b58d1fccee097a2b2c7
st-prasad <trindo.me@gmail.com>
2023-06-27 15:54:23 +0530

    removed react-spring code

--- app/components/Trending.tsx
+++ app/components/Trending.tsx
@@ -1,18 +1,19 @@
 "use client"
 import Image from 'next/image'
 import React from 'react'
-import { animated, useSpring } from 'react-spring'
+// import { animated, useSpring } from 'react-spring'
+
 
 
 const Trending = () => {
-    const styles = useSpring({
-        from: { opacity: "0" },
-        to: { opacity: "1" },
-        // config: { duration: "1500" },
-      })
+    // const styles = useSpring({
+    //     from: { opacity: "0" },
+    //     to: { opacity: "1" },
+    //     config: { duration: 2500 },
+    //   })
 
     return (
-        <div id='trending' className="flex  justify-around items-center p-20 gap-[2vw] bg-[#abc3cd] h-screen">
+        <div id='trending' className="flex justify-around items-center p-20 gap-[2vw] bg-[#abc3cd] h-screen">
 
             <div className="basis-1/3 bg-blue-950 min-w-[150px] max-w-[300px] rounded-lg shadow-lg w-full md:w-1/2 transform duration-500 hover:translate-x-5 hover:-translate-y-5 pointer-events-none relative z-10">
                 <Image className="w-full rounded-lg shadow-lg transform duration-500 hover:-translate-x-10 hover:translate-y-10 pointer-events-auto my-0 mx-auto"
@@ -34,7 +35,7 @@ const Trending = () => {
                 </ul>
             </div> */}
             {/* rewrite above comented div and add appropriate styles */}
-            <animated.div style={styles} className="text-center basis-2/3 md:text-left relative z-20">
+            <div className="text-center basis-2/3 md:text-left relative z-20">
                 <h1 className="text-4xl font-bold text-[#143aa2]">Top 5 trending AI technologies this month</h1>
                 <ul className="text-2xl font-semibold text-[#04060D]">
                     <li>GPT-3</li>
@@ -43,7 +44,7 @@ const Trending = () => {
                     <li>Google Brain</li>
                     <li>Deep Learning</li>
                 </ul>
-            </animated.div>
+            </div>
 
         </div>
     )
